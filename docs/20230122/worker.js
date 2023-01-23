a6130f95b4afc22debc69967065490092ed6cc27
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-01-23 17:40:55 -0500

    Create worker.js

---
+++ docs/20230122/worker.js
@@ -0,0 +1,74 @@
+/*
+(c) 2023 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+const asyncErrorLog = (async function () {
+  try {
+    const module = await import("https://scotwatson.github.io/Debug/Test/ErrorLog.mjs");
+    return module;
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncErrorLog ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+async function start( [ ErrorLog ] ) {
+  try {
+    let myPort;
+    self.addEventListener("message", mainMessageHandler);
+    self.addEventListener("messageerror", mainMessageErrorHandler);
+    self.postMessage("Hello");
+    self.setInterval(function () {
+      self.postMessage("tick");
+    }, 5000);
+    function mainMessageHandler(evt) {
+      switch (evt.data.cmd) {
+        case "port":
+          if (myPort === undefined) {
+            myPort = evt.data.port;
+            myPort.addEventListener("message", myMessageHandler);
+            myPort.addEventListener("messageerror", myMessageErrorHandler);
+            myPort.start();
+            self.postMessage("port received");
+          } else {
+            // Close and discard received port
+            evt.data.port.close();
+          }
+          break;
+        case "send":
+          myPort.postMessage("From Worker to Window");
+          break;
+        case "close":
+          myPort.close();
+          break;
+        default:
+          break;
+      };
+    }
+    function mainMessageErrorHandler(evt) {
+      self.postMessage(evt);
+    }
+    function myMessageHandler(evt) {
+      self.postMessage("Received: " + evt.data);
+    }
+    function myMessageErrorHandler(evt) {
+      self.postMessage("port message error");
+    }
+  } catch (e) {
+    ErrorLog.rethrow({
+      functionName: "start",
+      error: e,
+    });
+  }
+}
